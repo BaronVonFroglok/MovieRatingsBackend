ec77e83c772cb9d120622764aee30ca99f04884d
Chris Yanoso <christopheryanoso@gmail.com>
2021-02-11 11:57:42 -0500

    added controllers package and the start of the movie controller

---
+++ movieRatingApplication/src/main/java/com/revature/controllers/MovieController.java
@@ -0,0 +1,20 @@
+package com.revature.controllers;
+
+import com.revature.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+@CrossOrigin(origins = "http://localhost:4200")
+public class MovieController {
+
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+}
